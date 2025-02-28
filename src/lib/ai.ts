9b14f50ecfbbdd1423265d66b6a8f17cf70f511e
Tomas Lobato <tlobatodev@gmail.com>
2025-02-28 17:53:39 -0300

    fixed error in ai.ts

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -4,7 +4,8 @@ import { Message } from "../App"
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_KEY)
 const model = genAI.getGenerativeModel({model:"gemini-2.0-flash-lite"})
 
-export async function ask(prompt: string, context: Message[]) {
+export async function ask(prompt: string, context: Message) {
+  console.log(context)
   const result = await model.generateContent("instructions: answer briefly but not so much, possitively, and with seriousness. PROMPT: " + prompt)
   return result.response
 }
